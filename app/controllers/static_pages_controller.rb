08f8d5fc19852875f7862b769905db8b37d40ef7
kyuuki <kyuuki.japan@gmail.com>
2020-04-18 08:55:26 +0000

    ドキュメント修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def technology
+  end
+
+  def progress
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+
+  get 'technology', to: 'static_pages#technology'
+  get 'progress', to: 'static_pages#progress'
 end
